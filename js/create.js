c75cc2d19b58154c5f84e48dd02c75850f2c8c28
anitaHey <jiaa.anita@gmail.com>
2021-03-08 11:47:56 +0800

    Add file load

--- js/create.js
+++ js/create.js
@@ -1,2 +1,30 @@
+var init_text;
+var input_text;
+
 $(document).ready(function(e) {
+    $("#enter").click(async function() {
+        await loadFileAsText("init_upload", "init");
+        await loadFileAsText("input_upload", "input");
+    });
+
 });
+
+async function loadFileAsText(file_id, type) {
+    return new Promise((resolve, reject) => {
+        var fileToLoad = $('#' + file_id).prop('files')[0];
+
+        var fileReader = new FileReader();
+        fileReader.onload = function(fileLoadedEvent) {
+            var textFromFileLoaded = fileLoadedEvent.target.result;
+            if (type == "init")
+                init_text = textFromFileLoaded;
+            else if (type == "input")
+                input_text = textFromFileLoaded;
+
+            resolve(fileReader.result);
+        };
+        fileReader.onerror = reject;
+
+        fileReader.readAsText(fileToLoad, "UTF-8");
+    });
+}
